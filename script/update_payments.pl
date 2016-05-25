34d3c47d079e101bf7b9d814d788790c35e0f062
Jess Robinson <castaway@desert-island.me.uk>
2016-05-25 08:45:34 +0000

    Allow "SM" ref to be any number of digits

--- script/update_payments.pl
+++ script/update_payments.pl
@@ -70,7 +70,7 @@ sub import_payments {
     foreach my $trn (@$transactions) {
         next if(fiddle_payment($trn));
         next if($trn->{trnamt} <= 0);
-        next if($trn->{name} !~ /SM(\d{3,4})/);
+        next if($trn->{name} !~ /SM(\d+)/);
 
         # Is it a payment for/by a known member?
         my $id = $1;
