f936ccab07ecd1fcff193c86c790c5b352505e67
Jess Robinson <castaway@desert-island.me.uk>
2018-11-10 12:01:14 +0000

    New barclayscrape, slightly different params

--- script/from_bank.pl
+++ script/from_bank.pl
@@ -5,7 +5,7 @@ use DateTime;
 $ENV{BARCLAYSCRAPE} ||= '/usr/src/extern/barclayscrape';
 $ENV{ACCESS_HOME} ||= '/usr/src/extern/hackspace/AccessSystem';
 my $today = DateTime->now()->ymd();
-system("$ENV{BARCLAYSCRAPE}/get_ofx.js --otp=$ENV{CODE}");
+system("$ENV{BARCLAYSCRAPE}/barclayscrape.js --otp $ENV{CODE} get_ofx $ENV{ACCESS_HOME}");
 system("cp 20845883789160.ofx $ENV{ACCESS_HOME}/ofx/$today.ofx");
 system("scp $ENV{ACCESS_HOME}/ofx/$today.ofx pi\@inside.swindon-makerspace.org:AccessSystem/ofx/");
 system('ssh pi@inside.swindon-makerspace.org "cd /home/pi/AccessSystem; CATALYST_HOME=/home/pi/AccessSystem carton exec perl -Ilib /home/pi/AccessSystem/script/update_payments.pl"');
