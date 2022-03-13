ee950287cfe99d2329510e2720371f0dc8ca920e
Jess Robinson <castaway@desert-island.me.uk>
2022-03-13 19:06:34 +0000

    More "register" questions including vouchers + how member got here
    Also made the field descriptions less in-jokey

---
+++ AccessSystem-Schema-8.0-MySQL.sql
@@ -0,0 +1,212 @@
+--
+-- Created by SQL::Translator::Producer::MySQL
+-- Created on Sat Mar  5 18:10:41 2022
+--
+SET foreign_key_checks=0;
+
+DROP TABLE IF EXISTS membership_register;
+
+--
+-- Table: membership_register
+--
+CREATE TABLE membership_register (
+  name varchar(255) NOT NULL,
+  address text NOT NULL,
+  started_date date NOT NULL,
+  ended_date date NULL,
+  updated_date datetime NULL,
+  updated_reason text NOT NULL,
+  PRIMARY KEY (name, started_date)
+);
+
+DROP TABLE IF EXISTS people;
+
+--
+-- Table: people
+--
+CREATE TABLE people (
+  id integer NOT NULL auto_increment,
+  parent_id integer NULL,
+  name varchar(255) NOT NULL,
+  email varchar(255) NULL,
+  opt_in enum('0','1') NOT NULL DEFAULT '0',
+  dob varchar(7) NOT NULL,
+  address text NOT NULL,
+  how_found_us varchar(50) NULL,
+  github_user varchar(255) NULL,
+  telegram_username varchar(255) NULL,
+  telegram_chatid integer NULL,
+  google_id varchar(255) NULL,
+  concessionary_rate_override varchar(255) NULL DEFAULT '',
+  payment_override float NULL,
+  member_of_other_hackspace enum('0','1') NOT NULL DEFAULT '0',
+  voucher_code varchar(50) NULL,
+  voucher_start datetime NULL,
+  created_date datetime NOT NULL,
+  end_date datetime NULL,
+  INDEX people_idx_parent_id (parent_id),
+  PRIMARY KEY (id),
+  CONSTRAINT people_fk_parent_id FOREIGN KEY (parent_id) REFERENCES people (id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS tools;
+
+--
+-- Table: tools
+--
+CREATE TABLE tools (
+  id varchar(40) NOT NULL,
+  name varchar(255) NOT NULL,
+  assigned_ip varchar(15) NULL,
+  requires_induction enum('0','1') NOT NULL,
+  team varchar(50) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE name (name)
+) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS access_tokens;
+
+--
+-- Table: access_tokens
+--
+CREATE TABLE access_tokens (
+  id varchar(255) NOT NULL,
+  person_id integer NOT NULL,
+  type varchar(20) NOT NULL,
+  INDEX access_tokens_idx_person_id (person_id),
+  PRIMARY KEY (person_id, id),
+  CONSTRAINT access_tokens_fk_person_id FOREIGN KEY (person_id) REFERENCES people (id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS communications;
+
+--
+-- Table: communications
+--
+CREATE TABLE communications (
+  person_id integer NOT NULL,
+  sent_on datetime NOT NULL,
+  type varchar(50) NOT NULL,
+  status varchar(10) NOT NULL,
+  content text NOT NULL,
+  INDEX communications_idx_person_id (person_id),
+  PRIMARY KEY (person_id, sent_on),
+  CONSTRAINT communications_fk_person_id FOREIGN KEY (person_id) REFERENCES people (id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS dues;
+
+--
+-- Table: dues
+--
+CREATE TABLE dues (
+  person_id integer NOT NULL,
+  paid_on_date datetime NOT NULL,
+  expires_on_date datetime NOT NULL,
+  amount_p integer NOT NULL,
+  added_on datetime NOT NULL,
+  INDEX dues_idx_person_id (person_id),
+  PRIMARY KEY (person_id, paid_on_date),
+  CONSTRAINT dues_fk_person_id FOREIGN KEY (person_id) REFERENCES people (id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS login_tokens;
+
+--
+-- Table: login_tokens
+--
+CREATE TABLE login_tokens (
+  person_id integer NOT NULL,
+  login_token varchar(36) NOT NULL,
+  INDEX login_tokens_idx_person_id (person_id),
+  PRIMARY KEY (person_id, login_token),
+  CONSTRAINT login_tokens_fk_person_id FOREIGN KEY (person_id) REFERENCES people (id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS message_log;
+
+--
+-- Table: message_log
+--
+CREATE TABLE message_log (
+  tool_id varchar(40) NOT NULL,
+  message text NOT NULL,
+  from_ip varchar(15) NOT NULL,
+  written_date datetime NOT NULL,
+  INDEX message_log_idx_tool_id (tool_id),
+  PRIMARY KEY (tool_id, written_date),
+  CONSTRAINT message_log_fk_tool_id FOREIGN KEY (tool_id) REFERENCES tools (id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS transactions;
+
+--
+-- Table: transactions
+--
+CREATE TABLE transactions (
+  person_id integer NOT NULL,
+  added_on timestamp NOT NULL,
+  amount_p integer NOT NULL,
+  reason varchar(255) NOT NULL,
+  INDEX transactions_idx_person_id (person_id),
+  PRIMARY KEY (person_id, added_on),
+  CONSTRAINT transactions_fk_person_id FOREIGN KEY (person_id) REFERENCES people (id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS allowed;
+
+--
+-- Table: allowed
+--
+CREATE TABLE allowed (
+  person_id integer NOT NULL,
+  tool_id varchar(40) NOT NULL,
+  is_admin enum('0','1') NOT NULL,
+  added_on datetime NOT NULL,
+  INDEX allowed_idx_person_id (person_id),
+  INDEX allowed_idx_tool_id (tool_id),
+  PRIMARY KEY (person_id, tool_id),
+  CONSTRAINT allowed_fk_person_id FOREIGN KEY (person_id) REFERENCES people (id) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT allowed_fk_tool_id FOREIGN KEY (tool_id) REFERENCES tools (id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS tool_status;
+
+--
+-- Table: tool_status
+--
+CREATE TABLE tool_status (
+  id integer NOT NULL auto_increment,
+  tool_id integer NOT NULL,
+  entered_at datetime NOT NULL,
+  who_id integer NOT NULL,
+  status varchar(20) NOT NULL,
+  description text NOT NULL,
+  INDEX tool_status_idx_tool_id (tool_id),
+  INDEX tool_status_idx_who_id (who_id),
+  PRIMARY KEY (id),
+  CONSTRAINT tool_status_fk_tool_id FOREIGN KEY (tool_id) REFERENCES tools (id) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT tool_status_fk_who_id FOREIGN KEY (who_id) REFERENCES people (id)
+) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS usage_log;
+
+--
+-- Table: usage_log
+--
+CREATE TABLE usage_log (
+  person_id integer NULL,
+  tool_id varchar(40) NOT NULL,
+  token_id varchar(255) NOT NULL,
+  status varchar(20) NOT NULL,
+  accessed_date datetime NOT NULL,
+  INDEX usage_log_idx_person_id (person_id),
+  INDEX usage_log_idx_tool_id (tool_id),
+  PRIMARY KEY (tool_id, accessed_date),
+  CONSTRAINT usage_log_fk_person_id FOREIGN KEY (person_id) REFERENCES people (id) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT usage_log_fk_tool_id FOREIGN KEY (tool_id) REFERENCES tools (id)
+) ENGINE=InnoDB;
+
+SET foreign_key_checks=1;
+
+
