14d18b9a0818a82f67b887c667065de5899f49cd
Jess Robinson <castaway@desert-island.me.uk>
2023-02-18 16:50:14 +0000

    Enter missing weekend restriction data

--- sql/AccessSystem-Schema-11.0-12.0-PostgreSQL.sql
+++ sql/AccessSystem-Schema-11.0-12.0-PostgreSQL.sql
@@ -15,7 +15,7 @@ CREATE TABLE "tiers" (
 );
 
 INSERT INTO tiers (name, description, price, concessions_allowed) values ('MemberOfOtherHackspace', 'Living outside of Swindon Borough and a fully paid up member of another Maker or Hackspace', 500, 0);
-INSERT INTO tiers (name, description, price, restrictions) values ('Weekend', 'Access 12:00am Saturday until 12:00am Monday, and Wednesdays 6:30pm to 11:59pm only', 1500, '');
+INSERT INTO tiers (name, description, price, restrictions) values ('Weekend', 'Access 12:00am Saturday until 12:00am Monday, and Wednesdays 6:30pm to 11:59pm only', 1500, '{"times":[{"from":"3:18:00","to":"3:23:59"},{"from":"6:00:01","to":"6:23:59"},{"from":"7:00:01","to":"7:23:59"}]}');
 INSERT INTO tiers (name, description, price) values ('Standard', 'Access 24hours a day, 365 days a year', 2500);
 INSERT INTO tiers (name, description, price) values ('Sponsor', 'Access 24hours a day, 365 days a year', 3500);
 INSERT INTO tiers (name, description, price, dont_use) values ('MensShed', 'Members of Renew only, rate now retired', 1000, 1);
