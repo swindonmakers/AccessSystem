b9f8366686924a31fdd347dec51aed270546f967
Jess Robinson <castaway@desert-island.me.uk>
2024-08-10 12:51:40 +0100

    Update bank script to use new SSH port to connect to inside.

--- script/from_bank.pl
+++ script/from_bank.pl
@@ -7,5 +7,5 @@ $ENV{ACCESS_HOME} ||= '/usr/src/extern/hackspace/AccessSystem';
 my $today = DateTime->now()->ymd();
 system("$ENV{BARCLAYSCRAPE}/barclayscrape.js --otp $ENV{CODE} get_ofx $ENV{ACCESS_HOME}");
 system("cp 20845883789160.ofx $ENV{ACCESS_HOME}/ofx/$today.ofx");
-system("scp $ENV{ACCESS_HOME}/ofx/$today.ofx castaway\@inside.swindon-makerspace.org:/opt/AccessSystem/ofx/");
-system('ssh castaway@inside.swindon-makerspace.org "cd /opt/AccessSystem; CATALYST_HOME=/opt/AccessSystem carton exec perl -Ilib /opt/AccessSystem/script/update_payments.pl"');
+system("scp -P 2222 $ENV{ACCESS_HOME}/ofx/$today.ofx castaway\@inside.swindon-makerspace.org:/opt/AccessSystem/ofx/");
+system('ssh -p 2222 castaway@inside.swindon-makerspace.org "cd /opt/AccessSystem; CATALYST_HOME=/opt/AccessSystem carton exec perl -Ilib /opt/AccessSystem/script/update_payments.pl"');
