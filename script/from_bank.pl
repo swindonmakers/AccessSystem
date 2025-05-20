b45b2fe64266a3e9f64d10da6dd1858535e5d32e
Jess Robinson <castaway@desert-island.me.uk>
2025-05-20 09:22:22 +0100

    Run all the bank / payment stuff at once (no delays between import+email)

--- script/from_bank.pl
+++ script/from_bank.pl
@@ -9,3 +9,5 @@ system("$ENV{BARCLAYSCRAPE}/barclayscrape.js --otp $ENV{CODE} get_ofx $ENV{ACCES
 system("cp 20845883789160.ofx $ENV{ACCESS_HOME}/ofx/$today.ofx");
 system("scp -P 2222 $ENV{ACCESS_HOME}/ofx/$today.ofx castaway\@inside.swindon-makerspace.org:/opt/AccessSystem/ofx/");
 system('ssh -p 2222 castaway@inside.swindon-makerspace.org "cd /opt/AccessSystem; CATALYST_HOME=/opt/AccessSystem carton exec perl -Ilib /opt/AccessSystem/script/update_payments.pl"');
+system('ssh -p 2222 castaway@inside.swindon-makerspace.org "cd /opt/AccessSystem; CATALYST_HOME=/opt/AccessSystem carton exec perl -Ilib /opt/AccessSystem/script/membership_payments.pl"');
+system('ssh -p 2222 castaway@inside.swindon-makerspace.org "cd /opt/AccessSystem; CATALYST_HOME=/opt/AccessSystem carton exec perl -Ilib /opt/AccessSystem/script/send_communications.pl"');
